fb75c0a6224ee0d3611e73dc51e5d9c929ebff67
bherth <bherthchavez@gmail.com>
2022-10-27 16:30:23 +0800

    Done v1

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -6,7 +6,6 @@ import { arrayUnion, doc, updateDoc } from 'firebase/firestore'
 
 
 const Movie = ({item}) => {
-    const [saved, setSaved] =useState(false)
     const [like, setLike] =useState(false)
     const {user} = UserAuth()
 
@@ -15,9 +14,8 @@ const Movie = ({item}) => {
     const saveShow = async ()=>{
         if(user?.email){
             setLike(!like)
-            setSaved(true)
             await updateDoc(movieID, {
-                saveShow:arrayUnion({
+                savedShows:arrayUnion({
                     id: item.id,
                     title: item.title,
                     img: item.backdrop_path
@@ -33,7 +31,7 @@ const Movie = ({item}) => {
         <div className='w-[160px] sm:w-[200px] md:w-[240px] lg:[280px] inline-block cursor-pointer relative p-2'>
             <img className='w-full h-auto block' src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} alt={item?.title} />
 
-            <div className='absolute top-0 left-0 w-full h-full hover:bg-blac/80k opacity-0 hover:opacity-100 text-white'>
+            <div className='absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white'>
 
                 <p className='white-space-normal text-xm md:text-sm font-bold flex justify-center items-center h-full text-center'>{item?.title}</p>
                 <p onClick={saveShow}>
--- src/components/SavedShows.jsx
+++ src/components/SavedShows.jsx
@@ -3,6 +3,7 @@ import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 import { UserAuth } from "../context/AuthContext";
 import { db } from "../firebase";
 import { updateDoc, doc, onSnapshot } from "firebase/firestore";
+import {AiOutlineClose} from "react-icons/ai"
 
 const SavedShows = () => {
   const [movies, setMovies] = useState([]);
@@ -24,6 +25,21 @@ const SavedShows = () => {
     });
   }, [user?.email]);
 
+
+  const movieRef = doc(db, "users", `${user?.email}`);
+  const deleteShow = async (passedID) => {
+    try {
+      const result = movies.filter((item) => item.id !== passedID);
+      
+      await updateDoc(movieRef, {
+        savedShows: result,
+      });
+
+    }catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <h2 className="text-white font-bold md:text-xl p-4">My Shows</h2>
@@ -37,7 +53,8 @@ const SavedShows = () => {
           id={"slider"}
           className="w-full h-full overflow-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative"
         >
-          {movies.map((item, id) => (
+          {movies?.map((item, id) => ( 
+            
             <div
               key={id}
               className="w-[160px] sm:w-[200px] md:w-[240px] lg:[280px] inline-block cursor-pointer relative p-2"
@@ -48,10 +65,11 @@ const SavedShows = () => {
                 alt={item?.title}
               />
 
-              <div className="absolute top-0 left-0 w-full h-full hover:bg-blac/80k opacity-0 hover:opacity-100 text-white">
+              <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
                 <p className="white-space-normal text-xm md:text-sm font-bold flex justify-center items-center h-full text-center">
                   {item?.title}
                 </p>
+                <p onClick={()=> deleteShow(item?.id)} className="absolute top-4 right-4 text-gray-300"><AiOutlineClose /></p>
               </div>
             </div>
           ))}
